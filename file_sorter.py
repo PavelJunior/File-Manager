fd02e8fc9bca9dc77537f7fd3531f465311175cb
PavelJunior <pavelpyshpavel@gmail.com>
2020-03-20 10:33:12 -0700

    Created condition that checks whether folder was created by this program.

--- file_sorter.py
+++ file_sorter.py
@@ -3,6 +3,7 @@ import os
 import shutil
 import mimetypes
 import time
+import re
 from datetime import datetime, timedelta
 
 
@@ -36,7 +37,11 @@ def sort_files_with_dates(directory_path):
     week_in_seconds = 604800
 
     list_of_files = os.listdir(directory_path)
+    folder_name_pattern = r"^([0-9]{2} [A-Z][a-z]{2}) - ([0-9]{2} [A-Z][a-z]{2}) [0-9]{4}$"
+
     for file in list_of_files:
+        if re.search(folder_name_pattern, file):
+            continue
         file_path = os.path.join(directory_path, file)
         file_bd = os.stat(file_path).st_birthtime
         period_start_date_in_seconds = (file_bd // week_in_seconds * week_in_seconds) + timezone_difference - shift_to_monday
@@ -44,12 +49,17 @@ def sort_files_with_dates(directory_path):
         period_end_date = period_start_date + timedelta(days=7)
         folder_name = "{} - {} {}".format(period_start_date.strftime("%d %b"), period_end_date.strftime("%d %b"), period_end_date.year)
 
-        print(folder_name)
+        file_type = get_file_type(file)
+        related_timeline_path = os.path.join(directory_path, folder_name)
+        related_directory_path = os.path.join(related_timeline_path, file_type)
+        file_path = os.path.join(directory_path, file)
+        new_file_path = os.path.join(related_directory_path, file)
 
-
-
-    # directory_creation_timestamp = os.path.getctime()
-    # directory_creation_date = datetime.fromtimestamp(directory_creation_timestamp)
+        if not os.path.exists(related_timeline_path):
+            os.mkdir(related_timeline_path)
+        if not os.path.exists(related_directory_path):
+            os.mkdir(related_directory_path)
+        shutil.move(file_path, new_file_path)
 
 sort_files_with_dates('/Users/pavelpysenkin/Desktop/test/')
 
