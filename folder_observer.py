761612e74d18c7bc0e2ab669f59a899c9a7057b5
PavelJunior <pavelpyshpavel@gmail.com>
2020-06-14 11:39:53 -0700

    Implemented FolderObserver class inherited from FileSystemEventHandler.

---
+++ folder_observer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+from watchdog.events import FileSystemEventHandler
+from folder_organizer import FolderOrganizer
+
+
+class FolderObserver(FileSystemEventHandler):
+    def __init__(self, folder_path, sort_order=['d'], time_period='m', amount_of_periods=1):
+        self.folder_to_track = folder_path
+        self.sort_order = sort_order
+        self.time_period = time_period
+        self.amount_of_periods = amount_of_periods
+
+    def on_modified(self, event):
+        file_sorter = FolderOrganizer(self.folder_to_track)
+        file_sorter.sort_files(self.sort_order, self.time_period, self.amount_of_periods)
+
+
